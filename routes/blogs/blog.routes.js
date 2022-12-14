61541b1853534f6e81a2e3e55b71323a267fa36a
james4-daf <d.james@elsevier.com>
2022-12-14 17:49:51 +0000

    blog post pages done

--- routes/blogs/blog.routes.js
+++ routes/blogs/blog.routes.js
@@ -2,7 +2,7 @@ const BlogModel = require("../../models/blog.model");
 
 const router = require("express").Router();
 
-/* GET celebrity page page */
+/* GET blogs  page, i.e. list of all blogs*/
 router.get("/blogs", (req, res, next) => {
   BlogModel.find()
     .then((blogs) => {
@@ -10,6 +10,21 @@ router.get("/blogs", (req, res, next) => {
     })
     .catch((err) => {
       console.log("blogs fetch failed", err);
+    });
+});
+
+//get each blog detail page
+
+router.get("/blog/:id", (req, res, next) => {
+  const { id } = req.params;
+  //console.log("errrrrrrrrrrrr" + id);
+  BlogModel.findById(id.trim())
+    .then((blog) => {
+      res.render("blogs/blogPost.hbs", { blog });
+      console.log("Blog fetched", res);
+    })
+    .catch((err) => {
+      console.log("Blog fetch failed", err);
     });
 });
 
